15d1252ca5f6a80f19842100d3b210f9ea1da201
Mario Buikhuizen <mbuikhuizen@gmail.com>
2019-05-08 09:42:10 +0200

    change: Add extra scope to all style rules
    This will solve most style conflicts when vuetify is used within an
    existing page.

--- packages/vuetify/postcss.config.js
+++ packages/vuetify/postcss.config.js
@@ -1,11 +1,13 @@
 const autoprefixer = require('autoprefixer')
 const mqpacker = require('css-mqpacker')
+const scopify = require('postcss-scopify')
 
 module.exports = (ctx) => ({
   plugins: [
     autoprefixer({
       browsers: ['>0.5%', 'last 2 versions', 'not dead', 'not op_mini all']
     }),
+    scopify('.vuetify-styles'),
     mqpacker()
   ]
 })
