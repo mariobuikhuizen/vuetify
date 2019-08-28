d56d3a6421a8538aabcf712a1ad01412da21ba68
Mario Buikhuizen <mariobuikhuizen@gmail.com>
2019-08-28 12:02:06 +0200

    feat: split data-app functionality

--- packages/vuetify/src/mixins/detachable/index.ts
+++ packages/vuetify/src/mixins/detachable/index.ts
@@ -129,7 +129,7 @@ export default mixins<options &
       let target
       if (this.attach === false) {
         // Default, detach to app
-        target = document.querySelector('[data-app]')
+        target = document.querySelector('[vuetify-overlay]')
       } else if (typeof this.attach === 'string') {
         // CSS selector
         target = document.querySelector(this.attach)
@@ -139,7 +139,7 @@ export default mixins<options &
       }
 
       if (!target) {
-        consoleWarn(`Unable to locate target ${this.attach || '[data-app]'}`, this)
+        consoleWarn(`Unable to locate target ${this.attach || '[vuetify-overlay]'}`, this)
         return
       }
 
